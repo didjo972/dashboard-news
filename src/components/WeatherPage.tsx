d538d3e9879f26f68d3fcaf715b263eead90e18f
didjo972 <didjo972@gmail.com>
2018-06-06 22:44:25 +0200

    Missing files

---
+++ src/components/WeatherPage.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export interface IWeatherPageState {
+    content?: any;
+    timerID?: any;
+}
+
+export class WeatherPage extends Component<any, IWeatherPageState> {
+    private timerID: NodeJS.Timer;
+
+    constructor( props: any ) {
+        super( props );
+        this.state = {
+            content: (
+                <div
+                    id="cont_71bb8b0676a263d51918bb6ef59698f0"
+                    style={{
+                        backgroundColor: '#000000',
+                        border: '0px solid #D6D6D6',
+                        color: '#FFFFFF',
+                        fontFamily: 'Roboto',
+                        margin: '0 auto',
+                        width: '554px',
+                    }}
+                >
+                    <iframe
+                        id="71bb8b0676a263d51918bb6ef59698f0"
+                        style={{ width: '554px', color: '#FFFFFF', height: '195px' }}
+                        src="https://www.tameteo.com/getwid/71bb8b0676a263d51918bb6ef59698f0"
+                        frameBorder="0"
+                        scrolling="no"
+                        name="flipe"
+                    />
+                    <script
+                        type="text/javascript"
+                        async={true}
+                        src="https://www.tameteo.com/wid_loader/71bb8b0676a263d51918bb6ef59698f0"
+                    />
+                </div>
+            ),
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 3600000 );
+    }
+
+    componentWillUnmount() {
+        clearInterval( this.timerID );
+    }
+
+    tick() {
+        this.setState( prevState => {
+            return {
+                ...prevState,
+            };
+        } );
+    }
+
+    public render() {
+        return (
+            <div>
+                <label>Weather page</label>
+            </div>
+        );
+    }
+}
